c84f02004c9dda1c9a8f43131bb8a3d010f11c67
Adam Smith <asmith5144@hotmail.com>
2023-09-24 11:01:27 -0400

    <added Javascript to project>

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+   }
+/*
+if (topic === 'HTML') {
+    var shapes = ["triangle", "square", "pentagon", "circle"];
+    for(var x = 0; x < shapes.length; x++) {
+     console.log(shapes[x]);
+    };
+} else if (topic === 'CSS') {
+    var shapes = ["triangle", "square", "pentagon", "circle"];
+    for(var x = 0; x < shapes.length; x++) {
+     console.log(shapes[x]);
+    };
+} else if (topic === 'Git') {
+    var shapes = ["triangle", "square", "pentagon", "circle"];
+    for(var x = 0; x < shapes.length; x++) {
+     console.log(shapes[x]);
+    };
+} else if (topic === 'JavaScript') {
+    var shapes = ["triangle", "square", "pentagon", "circle"];
+    for(var x = 0; x < shapes.length; x++) {
+     console.log(shapes[x]);
+    };
+} else {
+  console.log('Please try again!');
+}
+*/
+
